d72ce5d12706a66b4ded98a9f230b0399e739e73
shamim 2000 <s40644812@gmail.com>
2025-08-29 20:37:49 +0600

    ✨ feat(user): implement user routes
    - create user routes for signup, login, and logout

---
+++ server/src/routes/user.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.route("/signup").post();
+router.route("/login").post();
+router.route("/logout").post();
+
+export default router;
