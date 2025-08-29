14b175a4f1afcb84c6e84d39329e61209ee84304
shamim 2000 <s40644812@gmail.com>
2025-08-29 20:45:16 +0600

    ✨ feat(user): implement user authentication controllers
    - create user signup controller
    - create user login controller
    - create user logout controller

---
+++ server/src/controllers/user.controllers.js
@@ -0,0 +1,8 @@
+import AsyncHandler from "../utils/AsyncHandler.js";
+import User from "../models/user.models.js";
+import ApiError from "../utils/ApiError.js";
+import ApiResponse from "../utils/ApiResponse.js";
+
+export const userSignup = AsyncHandler(async (req, res) => {});
+export const userLogin = AsyncHandler(async (req, res) => {});
+export const userLogout = AsyncHandler(async (req, res) => {});
