7e5dded7f6777755e86582ed5279d86abb79a20e
shamim 2000 <s40644812@gmail.com>
2025-08-29 20:45:06 +0600

    ✨ feat(user): implement user authentication routes
    - add signup route with avatar upload using multer
    - implement login and logout routes

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
 
+import {
+  userSignup,
+  userLogin,
+  userLogout,
+} from "../controllers/user.controllers.js";
+
+import { upload } from "../middleware/multer.middleware.js";
+
 const router = Router();
 
-router.route("/signup").post();
-router.route("/login").post();
-router.route("/logout").post();
+router.route("/signup").post(upload.single("avatar"), userSignup);
+router.route("/login").post(userLogin);
+router.route("/logout").post(userLogout);
 
 export default router;
