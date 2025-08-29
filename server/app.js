6271e0755f44b0ddcda1dc652724f9cf679ef32b
shamim 2000 <s40644812@gmail.com>
2025-08-29 20:37:39 +0600

    ✨ feat(server): implement user authentication routes
    - add user routes for signup, login, logout, and profile update
    - integrate cors, cookie-parser, and dotenv for enhanced security and configuration

--- server/app.js
+++ server/app.js
@@ -1,4 +1,30 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import userRouter from "./src/routes/user.routes.js";
+import dotenv from "dotenv";
 const app = express();
+
+dotenv.config({
+  path: "./.env", // load env variables
+});
+
+// use middlewares
+app.use(
+  cors({
+    origin: process.env.cors,
+    credentials: true,
+  })
+);
+app.use(
+  express.json({
+    limit: "100kb",
+  })
+);
+app.use(express.urlencoded({ extended: true, limit: "100kb" }));
+app.use(cookieParser());
+
+// routes
+app.use("/api/v1/users", userRouter);
 
 export { app };
