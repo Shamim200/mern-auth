de028812e280ceb63d40d6e6a62a96d8cd0f627e
shamim 2000 <s40644812@gmail.com>
2025-08-29 20:26:00 +0600

    ✨ feat(user): implement user model with authentication
    - define user schema with fields like fullname, username, email, password, and avatar
    - add password hashing using bcrypt before saving to the database
    - implement methods for password matching, access token generation, and refresh token generation

---
+++ server/src/models/user.models.js
@@ -0,0 +1,80 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+// User Schema
+const userSchema = new Schema(
+  {
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      min: [3, "please add at least 3 characters"],
+      max: 20,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      min: [3, "please add at least 3 characters"],
+      max: 20,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please add a valid email",
+      ],
+      max: 50,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: [8, "please add at least 8 characters"],
+      max: 20,
+    },
+    avatar: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+// hash password before saving to DB
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+//match user entered password to hashed password in database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+//jwt access token
+userSchema.methods.accessToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+// jwt refresh token
+userSchema.methods.refreshToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_REFRESH_SECRET, {
+    expiresIn: process.env.JWT_REFRESH_EXPIRE,
+  });
+};
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
