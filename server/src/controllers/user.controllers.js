c5c11dbd9801951246846a4d759e55007c6dbc17
shamim 2000 <s40644812@gmail.com>
2025-08-30 18:10:57 +0600

    ♻️ refactor(user): improve error and response handling
    - import ApiError and ApiResponse with destructuring
    - enhance code readability and maintainability

--- server/src/controllers/user.controllers.js
+++ server/src/controllers/user.controllers.js
@@ -1,7 +1,7 @@
 import AsyncHandler from "../utils/AsyncHandler.js";
 import User from "../models/user.models.js";
-import ApiError from "../utils/ApiError.js";
-import ApiResponse from "../utils/ApiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 export const userSignup = AsyncHandler(async (req, res) => {});
 export const userLogin = AsyncHandler(async (req, res) => {});
