40c1b0c17c2f59cf735c17b46889179888880888
shamim 2000 <s40644812@gmail.com>
2025-08-31 15:19:55 +0600

    🐛 fix(auth): implement auth middleware for logout route
    - apply AuthMiddleware to userLogout route
    - ensure only authenticated users can log out

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -7,11 +7,12 @@ import {
 } from "../controllers/user.controllers.js";
 
 import { upload } from "../middleware/multer.middleware.js";
+import { AuthMiddleware } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.route("/signup").post(upload.single("avatar"), userSignup);
 router.route("/signin").post(userSignin);
-router.route("/logout").post(userLogout);
+router.route("/logout").post(AuthMiddleware, userLogout);
 
 export default router;
