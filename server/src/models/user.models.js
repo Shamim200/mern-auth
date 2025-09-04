a92fb011f741a630fba6e1ea8272b364b4302dfa
shamim 200 <s40644812@gmail.com>
2025-09-04 13:18:28 +0600

    ✨ feat(user): add avatar field to user schema
    - add avatar field to user schema
    - update avatar field to be required

--- server/src/models/user.models.js
+++ server/src/models/user.models.js
@@ -40,8 +40,8 @@ const userSchema = new Schema(
       max: 20,
     },
     avatar: {
-      type: String,
-      default: "",
+      type: String, // url from cloudinary
+      required: true,
     },
     refreshToken: {
       type: String,
