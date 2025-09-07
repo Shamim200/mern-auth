f0072fedce8e914efcbfbca0fb304ed0a69656a2
shamim 200 <s40644812@gmail.com>
2025-09-07 11:48:14 +0600

    ✨ feat(headers): implement header component with auth navigation
    - create header component with sign-in and sign-up links
    - use react-bootstrap for styling and layout

---
+++ client/src/components/Headers.jsx
@@ -0,0 +1,39 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+const authList = [
+  {
+    id: 1,
+    name: "sign in",
+    links: "/sign-in",
+  },
+  {
+    id: 2,
+    name: "sign up",
+    links: "/sign-up",
+  },
+];
+const Headers = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src="./vite.svg" alt="" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto text-capitalize">
+            {authList.map((item) => {
+              const { id, name, links } = item;
+
+              return (
+                <Nav.Link href={links} key={id}>
+                  {name}
+                </Nav.Link>
+              );
+            })}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+export default Headers;
