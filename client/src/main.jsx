ce3414bcdf02a7197fd7cf87cc9cdb76b4916e5f
shamim 200 <s40644812@gmail.com>
2025-09-07 12:16:05 +0600

    ✨ feat(auth): implement authentication context for user management
    - add AuthProvider to wrap the App component
    - enable global access to authentication state and functions

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,14 @@ import "./App.css";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/Auth.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>
 );
