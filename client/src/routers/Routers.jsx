12646dddcce3d6a8cd5de74fb0d3d8520d719856
shamim 200 <s40644812@gmail.com>
2025-09-10 23:36:42 +0600

    ✨ feat(router): add dashboard route
    - add dashboard route to Routers.jsx
    - enable navigation to the dashboard page

--- client/src/routers/Routers.jsx
+++ client/src/routers/Routers.jsx
@@ -4,6 +4,7 @@ import Loading from "../components/Loading";
 
 const SignIn = lazy(() => import("../pages/SignIn"));
 const SignUp = lazy(() => import("../pages/SignUp"));
+const Dashboard = lazy(() => import("../pages/Dashboard"));
 const NotFound = lazy(() => import("../pages/NotFound"));
 
 const Routers = () => {
@@ -12,6 +13,7 @@ const Routers = () => {
       <Routes>
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Suspense>
