376619464a88743bcaec8152ffd871190c66abff
shamim 200 <s40644812@gmail.com>
2025-09-10 16:11:19 +0600

    ✨ feat(signup): implement user signup form with avatar upload
    - add form fields for fullname, username, email, and password
    - integrate avatar upload functionality
    - implement form submission to post data to the signup API endpoint
    - clear the form after successful signup

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,28 +1,99 @@
+import { useState, useRef } from "react";
 import { Container, Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const SignUp = () => {
+  const [fullname, setFullname] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [avatar, setAvatar] = useState(null);
+  const fileInputRef = useRef(null); // Add a ref for the file input
+
+  const handelSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+
+    formData.append("fullname", fullname);
+    formData.append("username", username);
+    formData.append("email", email);
+    formData.append("password", password);
+    formData.append("avatar", avatar);
+
+    try {
+      const response = await axios.post("/api/v1/users/signup", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      setFullname("");
+      setUsername("");
+      setEmail("");
+      setPassword("");
+      setAvatar(null);
+
+      if (fileInputRef.current) {
+        fileInputRef.current.value = ""; // Clear the file input
+      }
+
+      console.log(response);
+    } catch (error) {
+      console.log("Signup error: ", error?.message);
+    }
+  };
+
   return (
     <Container>
-      <Form>
+      <Form method="POST" onSubmit={handelSubmit}>
         <h4 className="text-center mt-5 text-capitalize">user sign up form</h4>
-        <Form.Group className="mb-3" controlId="basicname">
+        <Form.Group className="mb-3">
           <Form.Label className="text-capitalize">full name</Form.Label>
-          <Form.Control type="text" required />
+          <Form.Control
+            value={fullname}
+            onChange={(e) => setFullname(e.target.value)}
+            type="text"
+            required
+          />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="basicname">
+        <Form.Group className="mb-3">
           <Form.Label className="text-capitalize">user name</Form.Label>
-          <Form.Control type="text" required />
+          <Form.Control
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            type="text"
+            required
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="basicEmail">
           <Form.Label className="text-capitalize">Email</Form.Label>
-          <Form.Control type="email" required />
+          <Form.Control
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            required
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="basicPassword">
           <Form.Label className="text-capitalize">password</Form.Label>
-          <Form.Control type="password" required />
+          <Form.Control
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            required
+          />
+        </Form.Group>
+
+        {/* upload file */}
+
+        <Form.Group controlId="formFile" className="mb-3">
+          <Form.Control
+            onChange={(e) => setAvatar(e.target.files[0])}
+            ref={fileInputRef}
+            type="file"
+          />
         </Form.Group>
 
         <p className="text-capitalize">
