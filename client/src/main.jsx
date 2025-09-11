0d6ffd2b2be5fcfb07f6f0748e12e49d4c4a705a
shamim 200 <s40644812@gmail.com>
2025-09-11 14:23:56 +0600

    ♻️ refactor(main): remove strict mode
    - remove strict mode for smoother development experience

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./App.css";
 import App from "./App.jsx";
@@ -7,11 +6,11 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/Auth.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <>
     <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
     </BrowserRouter>
-  </StrictMode>
+  </>
 );
