7eae644bf64d5cbf8c6e896105aed7431218d57b
shamim 200 <s40644812@gmail.com>
2025-09-14 13:59:37 +0600

    ♻️ refactor(auth): simplify authentication protection logic
    - streamline authentication check using ternary operator for cleaner code

--- client/src/routers/Protect.jsx
+++ client/src/routers/Protect.jsx
@@ -2,9 +2,7 @@ import { UseAuth } from "../context/Auth";
 import { Navigate } from "react-router-dom";
 const Protect = ({ children }) => {
   const { user } = UseAuth();
-  if (!user) {
-    return <Navigate to="/signin" />;
-  }
-  return children;
+
+  return !user ? <Navigate to="/signin" /> : children;
 };
 export default Protect;
