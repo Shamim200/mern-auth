56bc888f1a1dc2ae92a30c1a53c7ff5a6ca85202
Shamim 200 <s40644812@gmail.com>
2026-06-08 21:46:15 +0600

    ✨feat(components): add sign up and sign in page

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,7 +1,25 @@
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
   return (
     <div className="container mx-auto mt-5">
       <h5 className="text-center">404 Not Found 😔!</h5>
+      <div className="d-flex justify-content-center align-items-center">
+        <Link to="/signup">
+          <Button
+            className="text-capitalize mx-3 my-5"
+            variant="outline-success"
+          >
+            sign up
+          </Button>
+        </Link>
+        <Link to="/signin">
+          <Button className="text-capitalize" variant="outline-success">
+            sign in
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 };
