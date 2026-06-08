2879a75cc2419e7dc166d615a113a9b0d9a60ef1
Shamim 200 <s40644812@gmail.com>
2026-06-08 21:26:58 +0600

    ♻️refector(db): remove dns and dns.setServer

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,13 +1,11 @@
 import mongoose from "mongoose";
-import dns from "dns";
-
-dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 export const connectDb = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("Connected to MongoDB");
+    console.log("Connected to MongoDB✅");
   } catch (error) {
-    console.log(error);
+    console.log(error.message);
+    process.exit(1);
   }
 };
