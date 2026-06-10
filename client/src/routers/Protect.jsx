a10a466fadd72d677cc91e7664eba9f9ff910a1e
Shamim 200 <s40644812@gmail.com>
2026-06-10 14:22:17 +0600

    ♻️refectore(Client/Route): remove child prop and add outlet, useLocation

--- client/src/routers/Protect.jsx
+++ client/src/routers/Protect.jsx
@@ -1,8 +1,13 @@
 import { UseAuth } from "../context/Auth";
-import { Navigate } from "react-router-dom";
-const Protect = ({ children }) => {
+import { Navigate, Outlet, Route, useLocation } from "react-router-dom";
+const Protect = () => {
   const { user } = UseAuth();
+  const location = useLocation();
 
-  return !user ? <Navigate to="/signin" /> : children;
+  return !user ? (
+    <Navigate to="/signin" state={{ from: location }} replace />
+  ) : (
+    <Outlet />
+  );
 };
 export default Protect;
