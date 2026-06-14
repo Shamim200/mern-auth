9c13891f6768bd6e6d02ac6c9007969c5d401e2f
Shamim 200 <s40644812@gmail.com>
2026-06-14 21:58:32 +0600

    ✅feat(auth): remove axios and add localstorage
    -Remove axios library and add default fetch api
    -Add and remove token to localstorage

--- client/src/context/Auth.jsx
+++ client/src/context/Auth.jsx
@@ -1,23 +1,38 @@
-import axios from "axios";
 import { createContext, useContext, useState, useEffect } from "react";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchUser = async () => {
       try {
         const token = localStorage.getItem("accessToken");
 
-        if (!token) return null;
+        if (!token) {
+          setLoading(false);
+          return;
+        }
 
-        const response = await axios.get(`/api/v1/users/dashboard`, {
+        // const response = await fetch(`/api/v1/users/dashboard`, {
+        //   headers: {
+        //     Authorization: `Bearer ${token}`,
+        //   },
+        const response = await fetch("/api/v1/users/dashboard", {
           headers: {
             Authorization: `Bearer ${token}`,
           },
         });
+
+        if (response.status === 401) {
+          localStorage.removeItem("accessToken");
+          setUser(null);
+          setLoading(false);
+          return;
+        }
+
         setUser(response.data.data);
       } catch {
         setUser(null);
@@ -27,13 +42,25 @@ export const AuthProvider = ({ children }) => {
   }, []);
 
   const userLogout = async () => {
-    await axios.post("/api/v1/users/logout");
-    localStorage.removeItem("accessToken");
-    setUser(null);
+    // await axios.post("/api/v1/users/logout");
+    // localStorage.removeItem("accessToken");
+    // setUser(null);
+    try {
+      await fetch("/api/v1/users/logout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      localStorage.removeItem("accessToken");
+      setUser(null);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
-    <AuthContext.Provider value={{ user, setUser, userLogout }}>
+    <AuthContext.Provider value={{ user, setUser, userLogout, loading }}>
       {children}
     </AuthContext.Provider>
   );
